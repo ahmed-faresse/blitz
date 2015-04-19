82d33981b87f8c91bb23b3d1346128dac737a75c
Geoffrey Heckmann <heckmann.geoffrey@epitech.eu>
2015-04-19 01:49:57 -0700

    add image event description fida and sc2

--- application/views/eventpage.php
+++ application/views/eventpage.php
@@ -96,22 +96,38 @@
     <div class="container main">
     <div class='row'>
         <div class='col-md-12'>
-          <div>
             <?php echo img($event->image_large_path, "center-block", "event"); ?>
             <div class="infos">
               
-              <h1><?php echo $event->name; ?></h1>
-              <p class="description"> <?php echo $event->description; ?></p>
-              <div class="date">
-              <i class="fa fa-gamepad"></i>
-              <p><?php echo $event->place; ?></p>
-              </div>
-              <div class="place">
-              <i class="fa fa-gamepad"></i>
-              <p><?php echo $event->date; ?></p>
-              </div>
+              
+              
+             
             </div>
-          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+            <h1><?php echo $event->name; ?></h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <p class="description"> <?php echo $event->description; ?></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-md-offset-2 date">
+            <i class="fa fa-map-marker"></i>
+            <p><?php echo $event->place; ?></p>
+        </div>
+        <div class="col-md-3 col-md-offset-2 place">
+            <i class="fa fa-clock-o"></i>
+            <p><?php echo date('F d, Y<\b\\r>h:mA', strtotime($event->date)) ; ?></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+            <progress max=<?php echo "$event->price_asked";?> value=<?php echo "$event->price_funded";?>></progress>
         </div>
       </div>
     </div>
