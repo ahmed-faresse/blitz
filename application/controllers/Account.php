aeb3dd3f835b4116008c5cf25db151f47dd93c3f
Alexis Le texier <Ataraxis@Alexiss-MBP.home>
2015-04-28 20:20:05 -0700

    Ajout de la partie my account + changement de mot de passe

---
+++ application/controllers/Account.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ataraxis
+ * Date: 4/27/15
+ * Time: 8:09 PM
+ */
---
+++ application/views/account.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ataraxis
+ * Date: 4/28/15
+ * Time: 5:21 PM
+ */
