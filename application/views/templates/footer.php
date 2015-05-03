8ac583cc512087311f78372ed369e6f08ed3dced
Geoffrey Heckmann <heckmann.geoffrey@epitech.eu>
2015-05-03 14:13:27 -0700

    achecker footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,8 +2,14 @@
                 echo '</div>' ;?> 
     <div id="footer" class="container">
     		<div class="row">
-    			<div class="col-md-12">
-        			    <? echo '<a href="https://validator.w3.org/check?uri=http%3A%2F%2Fblitz.besaba.com" target = "_blank">' . img("HTML5_Logo.png", "", "HTML5 Logo") . '</a>'; ?>
+    			<div class="col-md-2 col-md-offset-5">
+                    <div class="center-footer">
+        			    <? echo '<a href="https://validator.w3.org/check?uri=http%3A%2F%2Fblitz.besaba.com" target="_blank">' . img("HTML5_Logo.png", "", "HTML5 Logo") . '</a>'; ?>
+                        <div class="achecker">
+                            <a href="http://achecker.ca/checker/index.php" target="_blank"><p>WAI</p></a>
+                            <a href="http://achecker.ca/checker/index.php" target="_blank"><em class="fa fa-check-circle"></em></a>
+                        </div>
+                    </div>
         		</div>
         	</div>
     </div>
